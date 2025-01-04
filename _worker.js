386ef25c394fd087e5c9245fc085e1afb24806da
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-01-04 07:21:16 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,15 +6,11 @@ export default {
 
     const country = request.cf.country;
 
-    if (country != null && country in countryMap) {
-      const url = countryMap[country];
-      // Remove this logging statement from your final output.
-      console.log(
-        `Based on ${country}-based request, your user would go to ${url}.`,
-      );
-      return fetch(url, request);
+    // Check country and map it to the URL directly, or redirect if not found
+    if (country && countryMap[country]) {
+      return Response.redirect(countryMap[country], 302);
     } else {
-        return Response.redirect('https://whatsapp.com/channel/0029VaETuj089inlI5auYh0m', 302);
+      return Response.redirect('https://whatsapp.com/channel/0029VaETuj089inlI5auYh0m', 302);
     }
   },
 };
